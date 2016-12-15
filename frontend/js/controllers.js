a4874133dc7ae02dc36d32dd9f8f08fb39cd3292
Aymane Khouaji <akhouaji@linagora.com>
2016-12-15 14:40:16 +0100

    Delete helloworld funtcion

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -1,8 +1,6 @@
 'use strict';
 
 angular.module('linagora.esn.cryptpad')
-  .controller('cryptpadController', function($scope, getHelloWorld) {
-    getHelloWorld.then(function(message) {
-      $scope.message = message;
-    });
+  .controller('cryptpadController', function() {
+
   });
--- frontend/js/services.js
+++ frontend/js/services.js
@@ -1,8 +1,3 @@
 'use strict';
 
-angular.module('linagora.esn.cryptpad')
-  .factory('getHelloWorld', function($http) {
-    return $http.get('/cryptpad/api/sayhello').then(function(response) {
-      return response.data.message;
-    });
-  });
+angular.module('linagora.esn.cryptpad');
