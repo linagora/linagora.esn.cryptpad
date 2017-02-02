e55bbb51c89aa84a248220ddfff76102c0988fea
Aymane Khouaji <akhouaji@linagora.com>
2017-02-02 10:46:52 +0100

    Refactoring and add some utils function

--- frontend/app/services/utils.service.js
+++ frontend/app/services/utils.service.js
@@ -6,8 +6,11 @@
 
     function CryptpadUtils() {
       let service = {
-        hexToBase64: hexToBase64
-      }
+        hexToBase64: hexToBase64,
+        base64ToHex: base64ToHex,
+        parsePadUrl: parsePadUrl,
+        parseHash: parseHash
+      };
 
       return service;
 
@@ -19,6 +22,44 @@
               .split(" ");
           var byteString = String.fromCharCode.apply(null, hexArray);
           return window.btoa(byteString).replace(/\//g, '-').slice(0,-2);
+      }
+
+      function base64ToHex(b64String) {
+          var hexArray = [];
+          atob(b64String.replace(/-/g, '/')).split("").forEach(function(e){
+              var h = e.charCodeAt(0).toString(16);
+              if (h.length === 1) { h = "0"+h; }
+              hexArray.push(h);
+          });
+          return hexArray.join("");
+      }
+
+      function parsePadUrl(href) {
+          var patt = /^https*:\/\/([^\/]*)\/(.*?)\//i;
+
+          var ret = {};
+          var hash = href.replace(patt, function (a, domain, type, hash) {
+              ret.domain = domain;
+              ret.type = type;
+              return '';
+          });
+          ret.hash = hash.replace(/#/g, '');
+          return ret;
+      }
+
+      function parseHash(hash) {
+        //Hash the hash of window.location.hash
+        // #/cryptpad/1/edit/{chanKey}/{cryptKey}
+          var parsed = {};
+          var hashArr = hash.split('/');
+          if (hashArr[2] && hashArr[2] === '1') {
+              parsed.version = 1;
+              parsed.mode = hashArr[3];
+              parsed.channel = hashArr[4];
+              parsed.key = hashArr[5];
+              return parsed;
+          }
+          return;
       }
     }
 })();
