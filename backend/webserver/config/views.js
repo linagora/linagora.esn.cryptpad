ea54f005fe430207921497bbb1e454060111b7ef
Aymane Khouaji <akhouaji@linagora.com>
2017-02-02 10:39:36 +0100

    Modify the config of webserver

--- backend/webserver/config/views.js
+++ backend/webserver/config/views.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var express = require('express');
-var FRONTEND_PATH = require('../constants').FRONTEND_PATH;
+const express = require('express');
+const FRONTEND_PATH = require('../constants').FRONTEND_PATH;
+const CORE_FRONTEND_PATH = require('../constants').CORE_FRONTEND_PATH;
 
 module.exports = function(dependencies, application) {
   application.use(express.static(FRONTEND_PATH));
   application.set('views', FRONTEND_PATH + '/app');
   application.get('/app/*', function(req, res) {
-    var templateName = req.params[0].replace(/\.html$/, '');
-    res.render(templateName);
+    res.render(req.params[0].replace(/\.html$/, ''), { basedir: CORE_FRONTEND_PATH + '/views' });
   });
 };
